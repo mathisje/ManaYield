2d7aad72fc1ec91409bdee74c8f74c83c9094358
Jeff <mathisjeffr@gmail.com>
2017-11-21 03:03:29 -0500

    add tabs and some css adjustments

--- mana-yield/src/app.js
+++ mana-yield/src/app.js
@@ -2,18 +2,43 @@ import React, { Component } from 'react';
 import './css/app.css';
 import './css/mana.css';
 
+import AboutTheApp from "./components/aboutTheApp";
 import DeckGenerator from './components/deckGenerator.js'
+import TabContainer from "./components/tabContainer";
 
 class App extends Component {
 
+  _generateTabList = () => {
+    let appTab = {
+      title: 'Deck List',
+      content: (
+        <div key={0}>
+          <DeckGenerator />
+        </div>
+      )
+    };
+    let aboutTab = {
+      title: 'About',
+      content: (
+        <div key={1}>
+          <AboutTheApp />
+        </div>
+      )
+    };
+    return [appTab, aboutTab];
+  };
+
   render() {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <h1 className='App-title'>Mana Yield</h1>
-        </header>
-        <DeckGenerator />
-      </div>
+        <TabContainer containerClass='App'
+                      contentContainerClass='content-container'
+                      tabRowClass='tab-row-class'
+                      headerClass='tab-header'
+                      selectedHeaderClass='tab-header-selected'
+                      titleClass='tab-title'
+                      defaultIndex={0}
+                      emptySpaceRight={(<div className='empty-tab' />)}
+                      tabList={this._generateTabList} />
     )
   }
 }
---
+++ mana-yield/src/components/aboutTheApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutTheApp = function(props) {
+  return (
+    <div className='about'>
+      <p>
+        This is text about the app.
+      </p>
+    </div>
+  )
+};
+
+export default AboutTheApp;
