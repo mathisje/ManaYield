cf56e67330e593487418b732cf975b55742d9ace
Jeff <mathisjeffr@gmail.com>
2017-11-30 03:37:43 -0500

    simplified file structure

--- mana-yield/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './app.js';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
